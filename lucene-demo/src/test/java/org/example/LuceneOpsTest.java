42a2ea4e2774557eb084f070d7f6ff21eccc2a2f
m0v1 <mail@mail>
2024-11-05 15:48:00 +0800

    test: 新增搜索用例

--- lucene-demo/src/test/java/org/example/LuceneOpsTest.java
+++ lucene-demo/src/test/java/org/example/LuceneOpsTest.java
@@ -5,11 +5,19 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.example.dto.Book;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -101,6 +109,50 @@ public class LuceneOpsTest {
         // D:\temp\lucene\index\_0.si
         // D:\temp\lucene\index\segments_1
 
+    }
+
+    /**
+     * 搜索
+     */
+    @Test
+    public void testSearchIndex() throws Exception {
+        // 先执行索引创建
+        testCreateIndex();
+
+        // 1. 创建Query搜索对象
+        // 创建分词器
+        Analyzer analyzer = new StandardAnalyzer();
+        // 创建搜索解析器，第一个参数：默认Field域，第二个参数：分词器
+        QueryParser queryParser = new QueryParser("id", analyzer);
+        // 创建搜索对象
+        Query query = queryParser.parse("desc:java OR name:solr");
+        // 2. 创建Directory流对象,声明索引库位置
+        Directory directory = FSDirectory.open(Paths.get(LUCENE_INDEX));
+        // 3. 创建索引读取对象IndexReader
+        IndexReader reader = DirectoryReader.open(directory);
+        // 4. 创建索引搜索对象
+        IndexSearcher searcher = new IndexSearcher(reader);
+        // 5. 使用索引搜索对象，执行搜索，返回结果集TopDocs
+        // 第一个参数：搜索对象，第二个参数：返回的数据条数，指定查询结果最顶部的n条数据返回
+        TopDocs topDocs = searcher.search(query, 10);
+        System.out.println("查询到的数据总条数是：" + topDocs.totalHits);
+        // 获取查询结果集
+        ScoreDoc[] docs = topDocs.scoreDocs;
+        // 6. 解析结果集
+        for (ScoreDoc scoreDoc : docs) {
+            // 获取文档
+            int docID = scoreDoc.doc;
+            Document doc = searcher.doc(docID);
+            System.out.println("=============================");
+            System.out.println("score:" + scoreDoc.score);
+            System.out.println("docID:" + docID);
+            System.out.println("bookId:" + doc.get("id"));
+            System.out.println("name:" + doc.get("name"));
+            System.out.println("price:" + doc.get("price"));
+            System.out.println("desc:" + doc.get("desc"));
+        }
+        // 释放资源
+        reader.close();
     }
 
     /**
