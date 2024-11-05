065efe487078bbcc116b68fecabc0f6eafe9f786
m0v1 <mail@mail>
2024-11-05 15:39:43 +0800

    test: 新增针对索引目录进行预删除的前置处理

--- lucene-demo/src/test/java/org/example/LuceneOpsTest.java
+++ lucene-demo/src/test/java/org/example/LuceneOpsTest.java
@@ -10,13 +10,24 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.example.dto.Book;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LuceneOpsTest {
+
+    public static final String LUCENE_INDEX = "D:/temp/lucene/index";
+
+    @Before
+    public void setUp() throws Exception {
+        // clean directory
+        File directory = new File(LUCENE_INDEX);
+        deleteDirectory(directory);
+    }
 
     /**
      * 创建索引
@@ -32,19 +43,25 @@ public class LuceneOpsTest {
         bookOne.setId(1);
         bookOne.setName("Lucene");
         bookOne.setPrice(100.45F);
-        bookOne.setDesc("Lucene Core is a Java library providing powerful indexing and search features,as well as spellchecking, hit highlighting and advanced analysis/tokenization capabilities. The PyLucene sub project provides Python bindings for Lucene Core.");
+        bookOne.setDesc("Lucene Core is a Java library providing powerful indexing and search features," +
+                "as well as spellchecking, hit highlighting and advanced analysis/tokenization capabilities. " +
+                "The PyLucene sub project provides Python bindings for Lucene Core.");
 
         Book bookTwo = new Book();
         bookTwo.setId(11);
         bookTwo.setName("Solr");
         bookTwo.setPrice(320.45F);
-        bookTwo.setDesc("Solr is highly scalable, providing fully fault tolerant distributed indexing, search and analytics. It exposes Lucene's features through easy to use JSON/HTTP interfaces or native clients for Java and other languages.");
+        bookTwo.setDesc("Solr is highly scalable, providing fully fault tolerant distributed indexing, " +
+                "search and analytics. It exposes Lucene's features through easy to use JSON/HTTP interfaces or " +
+                "native clients for Java and other languages.");
 
         Book bookThree = new Book();
         bookThree.setId(21);
         bookThree.setName("Hadoop");
         bookThree.setPrice(620.45F);
-        bookThree.setDesc("The Apache Hadoop software library is a framework that allows for the distributed processing of large data sets across clusters of computers using simple programming models.");
+        bookThree.setDesc("The Apache Hadoop software library is a framework that allows for " +
+                "the distributed processing of large data sets across clusters of computers " +
+                "using simple programming models.");
 
         bookArrayList.add(bookOne);
         bookArrayList.add(bookTwo);
@@ -65,7 +82,7 @@ public class LuceneOpsTest {
         // 3.创建Analyzer分词器,分析文档，对文档进行分词
         Analyzer analyzer = new StandardAnalyzer();
         // 创建Directory对象,声明索引库的位置
-        Directory directory = FSDirectory.open(Paths.get("D:/temp/lucene/index"));
+        Directory directory = FSDirectory.open(Paths.get(LUCENE_INDEX));
         // 创建IndexWriteConfig对象，写入索引需要的配置
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         // 4.创建IndexWriter写入对象 添加文档对象document
@@ -75,6 +92,7 @@ public class LuceneOpsTest {
         }
         // 释放资源
         indexWriter.close();
+        System.out.println("索引创建成功");
 
         // 执行方法后指定目录生成的文件如下
         // D:\temp\lucene\index\write.lock
@@ -83,5 +101,32 @@ public class LuceneOpsTest {
         // D:\temp\lucene\index\_0.si
         // D:\temp\lucene\index\segments_1
 
+    }
+
+    /**
+     * 删除指定目录
+     *
+     * @param dir 待删除的目录
+     */
+    private void deleteDirectory(File dir) {
+        if (dir == null || !dir.exists()) {
+            System.out.println("目录不存在: " + dir);
+            return;
+        }
+
+        // 递归删除目录内容
+        if (dir.isDirectory()) {
+            File[] files = dir.listFiles();
+            // 防止空指针异常
+            if (files != null) {
+                for (File file : files) {
+                    // 递归删除子文件和目录
+                    deleteDirectory(file);
+                }
+            }
+        }
+
+        // 删除当前目录或文件
+        dir.delete();
     }
 }
