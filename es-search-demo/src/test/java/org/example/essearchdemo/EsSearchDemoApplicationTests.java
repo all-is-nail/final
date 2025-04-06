41694ba9468ab7a2a4273e854e7447c2108b7d84
m0v1 <mail@atomoperate.com>
2025-04-06 19:31:10 +0800

    test: add initial test class for EsSearchDemoApplication with context load verification

---
+++ es-search-demo/src/test/java/org/example/essearchdemo/EsSearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.essearchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsSearchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
