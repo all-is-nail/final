eec47cbc724481615c12661548d1b864d9254ac8
m0v1 <mail@atomoperate.com>
2025-04-06 20:39:08 +0800

    feat: add search functionality to BookRepository

--- es-search-demo/src/main/java/org/example/essearchdemo/repository/BookRepository.java
+++ es-search-demo/src/main/java/org/example/essearchdemo/repository/BookRepository.java
@@ -4,6 +4,16 @@ import org.example.essearchdemo.model.Book;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends ElasticsearchRepository<Book, Long> {
+
+    /**
+     * search by name
+     * 
+     * @param name
+     * @return
+     */
+    List<Book> findByNameContainingIgnoreCase(String name);
 }
