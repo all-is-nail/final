5b3945ec5f14d56bcb68b3d55d6b3f2e522b1025
m0v1 <mail@mail>
2025-06-11 18:45:32 +0800

    test: add unit test for BigDecimal operations demonstrating precision, scale, and arithmetic

--- base/src/test/java/org/example/common/CommonTest.java
+++ base/src/test/java/org/example/common/CommonTest.java
@@ -35,4 +35,31 @@ public class CommonTest {
 
         System.out.println(propertiesMap);
     }
+
+    /**
+     * Test BigDecimal operations with different precision
+     */
+    @Test
+    public void testBigDecimal() {
+        // Create BigDecimal instances with different precision and scale
+        java.math.BigDecimal bd1 = new java.math.BigDecimal("10.555");
+        java.math.BigDecimal bd2 = new java.math.BigDecimal("2.33");
+        System.out.println("bd1 precision is " + bd1.precision());
+        System.out.println("bd1 scale is " + bd1.scale());
+        System.out.println("bd2 precision is " + bd2.precision());
+        System.out.println("bd2 scale is " + bd2.scale());
+
+        // Perform arithmetic operations with different precision
+        System.out.println("Addition: " + bd1.add(bd2));
+        System.out.println("Subtraction: " + bd1.subtract(bd2));
+        System.out.println("Multiplication: " + bd1.multiply(bd2));
+        // Division with scale and rounding mode
+        System.out.println("Division (2 decimal places): " + 
+            bd1.divide(bd2, 2, java.math.RoundingMode.HALF_UP));
+        System.out.println("Division (3 decimal places): " + 
+            bd1.divide(bd2, 3, java.math.RoundingMode.HALF_UP));
+
+        // Compare BigDecimals
+        System.out.println("Comparison: " + bd1.compareTo(bd2));
+    }
 }
